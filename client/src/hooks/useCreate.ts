d9807555cc6b03a9123dd73cc4d19a4eb8ebc91a
Eytan3000 <eytankrief@gmail.com>
2024-09-01 08:36:05 +0300

    removed comment

--- client/src/hooks/useCreate.ts
+++ client/src/hooks/useCreate.ts
@@ -4,11 +4,5 @@ import { createInDb } from '../API';
 export const useCreate = () => {
   return useMutation({
     mutationFn: createInDb,
-    // onSuccess: () => {
-    //   setAlert('Saved!');
-    // },
-    // onError: () => {
-    //   setAlert('There was a problem');
-    // },
   });
 };
